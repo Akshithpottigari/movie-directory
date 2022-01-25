c981e2db07dbc76f239eafbb467d52ed80a42a47
AkshithPOttigari <akshithpottigari@gmail.com>
2022-01-25 19:12:13 +0530

    uplaoding

--- public/index.js
+++ public/index.js
@@ -1,36 +1,15 @@
-fetch('movies.json')
-.then(response => response.json())
-.then(data => {
+fetch("movies.json")
+  .then((response) => response.json())
+  .then((data) => {
     // console.log(data[0].title)
     // document.querySelector("#demo").innerHTML = data.id;
-    for (let i = 0; i < data.length; i++){
-        // console.log(data[i].title)
-        document.querySelector("#movie-container").innerHTML += `
-        <div id="movie">
-            <img class = "poster" src="${data[i].posterurl}" alt="poster url">
-            <div id="id">
-                Id : ${data[i].id}
+    for (let i = 0; i < data.length; i++) {
+      // console.log(data[i].title)
+      document.querySelector("#movie").innerHTML += `
+      <div class="movie-overlay"></div>
+      <div class="movie-image">
+                <img src="${data[i].posterurl}" alt="">
             </div>
-            <div id = "movie-title">
-                Title : ${data[i].title}
-            </div>
-            <div id = "movie-poster">
-            </div>
-            <div id = "movie-year">
-                Year: ${data[i].year}
-            </div>
-            <div id = "movie-genres">
-                Genres ${data[i].genres}
-            </div>
-            <div id = "movie-actors">
-                Actors ${data[i].actors}
-            </div>
-            <div id = "movie-releaseDate">
-                Release Date :  ${data[i].releaseDate}
-            </div>
-        </div>
         `;
-
-
     }
-})
+  });
